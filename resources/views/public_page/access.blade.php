34e9795bc8ce6f21ce9733e9d7ba2bc94ded8dd1
TAKATSU MARINA <75340313+marina-t1123@users.noreply.github.com>
2023-07-17 15:30:46 +0900

    feat:チャプター６の共通ヘッダー作成、TOPページとアクセス案内ページ作成

---
+++ resources/views/public_page/access.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.costom-app')
+
+@section('content')
+    <div class="container">
+        <h1 clas="text-center">アクセス案内ページ</h1>
+        <p clas="text-center">現在、アクセス案内ページ作成中。</p>
+    </div>
+@endsection
